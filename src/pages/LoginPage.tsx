ab4c1cbdd8815a84a4898de504a1e321dc8e3784
Pontus <ekberg.pontus@hotmail.se>
2024-11-21 12:07:24 +0100

    loginpage that workt with firebase

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,68 @@
-const LoginPage = () => {
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../service/Firebase";
+import claus from "../assets/img/Claus.png";
+
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Login successful!");
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("An unexpected error occurred");
+      }
+    }
+  };
+
   return (
-    <div className="overflow-hidden h-screen">
-      <h1 className="flex justify-center mt-10 text-white font-bold text-4xl">
-        Login
-      </h1>
+    <div className="relative overflow-hidden h-screen flex flex-col items-center">
+      <h1 className="text-white font-bold text-4xl mt-10 mb-[50px]">Login</h1>
+      <form
+        onSubmit={handleLogin}
+        className="flex flex-col items-center w-80  p-6 "
+      >
+        {error && <p className="text-red-600 text-sm mb-4">{error}</p>}
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-2 mb-4 border border-black"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-2 mb-4 border border-black"
+          required
+        />
+        <button
+          type="submit"
+          className="w-36 bg-gray-300 py-2 border-2 border-black hover:bg-blue-600 transition"
+        >
+          Login
+        </button>
+      </form>
+      <button className="mt-[250px] z-20 bg-gray-300 border-2 border-black w-32 h-10">
+        <a href="signup">SignUp</a>
+      </button>
+      <img
+        className="absolute z-10 top-[350px] right-[125px] -rotate-[8deg]"
+        src={claus}
+        alt=""
+      />
     </div>
   );
 };
