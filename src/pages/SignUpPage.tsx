a8597be4078262c0e076c95cebd526e7859a2372
Pontus <ekberg.pontus@hotmail.se>
2025-01-06 16:38:34 +0100

    changed style signupPage

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -2,10 +2,9 @@ import React, { useState } from "react";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../service/Firebase";
 import { useNavigate } from "react-router-dom";
-import beerpink from "../assets/img/Beerpink.png";
-import signupimg from "../assets/img/Planksignup.png";
-import faceimg from "../assets/img/Faceyellow.png";
-import skumol from "../assets/img/skumol.png";
+//import beerpink from "../assets/img/Beerpink.png";
+import plank from "../assets/img/Plank.png";
+//import faceimg from "../assets/img/Faceyellow.png";
 
 const SignUpPage: React.FC = () => {
   const [email, setEmail] = useState<string>("");
@@ -50,8 +49,8 @@ const SignUpPage: React.FC = () => {
   };
 
   return (
-    <div className="relative overflow-hidden h-screen flex flex-col items-center font-barrio">
-      <img className="mx-auto w-[200px] md:w-[350px]" src={signupimg} alt="" />
+    <div className="relative overflow-hidden h-screen flex flex-col items-center font-sourgummy">
+      <img className="mx-auto w-[200px] md:w-[350px]" src={plank} alt="" />
       <form
         onSubmit={handleSignUp}
         className="flex flex-col relative items-center w-80 p-6 md:mt-36"
@@ -63,7 +62,7 @@ const SignUpPage: React.FC = () => {
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full z-20 opacity-85 p-2 mb-4 border border-black lg:w-80 lg:h-14"
+          className="w-full z-20 opacity-85 p-3 mb-4 rounded-lg lg:w-80 lg:h-14"
           required
         />
         <input
@@ -71,7 +70,7 @@ const SignUpPage: React.FC = () => {
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="w-full z-20 opacity-85 p-2 mb-4 border border-black lg:w-80 lg:h-14"
+          className="w-full z-20 opacity-85 p-3 mb-4 rounded-lg lg:w-80 lg:h-14"
           required
         />
         <input
@@ -79,19 +78,17 @@ const SignUpPage: React.FC = () => {
           placeholder="Confirm Password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
-          className="w-full z-20 opacity-85 p-2 mb-4 border border-black lg:w-80 lg:h-14"
+          className="w-full z-20 opacity-85 p-3 mb-4 rounded-lg lg:w-80 lg:h-14"
           required
         />
         <button
           type="submit"
-          className="w-36 z-20 bg-cover bg-center drop-shadow-md hover:scale-105 hover:brightness-110 rounded-lg py-2 border-2 border-black lg:w-48 lg:h-14 text-xl"
-          style={{
-            backgroundImage: `url(${skumol})`,
-          }}
+          className="bg-yellow-400 opacity-90 h-16 w-40 z-20 drop-shadow-md hover:scale-105 hover:brightness-110 rounded-lg py-2 lg:w-48 lg:h-14 text-white text-2xl"
         >
           Sign Up
         </button>
       </form>
+      {/*
       <img
         className="absolute -bottom-[50px] -right-[85px] md:w-[500px] lg:w-[600px]"
         src={beerpink}
@@ -102,6 +99,7 @@ const SignUpPage: React.FC = () => {
         src={faceimg}
         alt=""
       />
+      */}
     </div>
   );
 };
